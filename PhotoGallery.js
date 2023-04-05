290b8f0978c97ecb37b4ddb8fe0afcd257945607
Kaz3r0 <kasis.ellisjr@gmail.com>
2023-04-05 12:29:24 -0300

    Creators Phone and Tablet

---
+++ PhotoGallery.js
@@ -0,0 +1,18 @@
+var photos = document.querySelectorAll('.photo');
+var modal = document.createElement('div');
+modal.classList.add('modal');
+document.body.appendChild(modal);
+
+photos.forEach(function(photo) {
+  photo.addEventListener('click', function() {
+    var modalContent = document.createElement('img');
+    modalContent.src = this.src;
+    modal.appendChild(modalContent);
+    modal.style.display = 'block';
+  });
+});
+
+modal.addEventListener('click', function() {
+  modal.style.display = 'none';
+  modal.innerHTML = '';
+});
