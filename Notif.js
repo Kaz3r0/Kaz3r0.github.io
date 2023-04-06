f3910984daf145f7c85d28bfd504f57d2a8de00f
Kaz3r0 <kasis.ellisjr@gmail.com>
2023-04-06 07:30:32 -0300

    the few last changes

--- Notif.js
+++ Notif.js
@@ -1,20 +1,6 @@
-document.getElementById("submit").addEventListener("click", function() {
-  submitRating();
-  document.querySelector('form').reset();
-});
+const submitBtn = document.getElementById('submit-btn');
 
-function submitRating() {
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-  else if (Notification.permission === "granted") {
-    var notification = new Notification("Thank You for Rating!");
-  }
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      if (permission === "granted") {
-        var notification = new Notification("Thank You for Rating!");
-      }
-    });
-  }
-}
+submitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  alert(`Thank you for submitting the form!`);
+});
