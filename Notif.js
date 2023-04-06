462fd3eb7fd34b27a3da56d46b5953706328059d
Kaz3r0 <kasis.ellisjr@gmail.com>
2023-04-06 02:04:50 -0300

    notification check

--- Notif.js
+++ Notif.js
@@ -1,13 +1,20 @@
-function submitRating() {
-  
-  const button = document.query Selector("button")
-  const notification = 
+document.getElementById("submit").addEventListener("click", function() {
+  submitRating();
+  document.querySelector('form').reset();
+});
 
-  button.addEventListener("click", () =>{
-	  Notification.requestPermission().then(perm =>{
-		  if (perm === "granted") {
-			new Notification('Thank You for Rating!');
-		  }
-	  })
-  })
+function submitRating() {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+  else if (Notification.permission === "granted") {
+    var notification = new Notification("Thank You for Rating!");
+  }
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Thank You for Rating!");
+      }
+    });
+  }
 }
